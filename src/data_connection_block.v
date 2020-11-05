7a6fc23bc9f4e9b7b7cf1b07df052875bdf40911
Arya Reais-Parsi <himself@aryaparsi.com>
2020-11-04 17:35:31 -0800

    add comments

--- src/data_connection_block.v
+++ src/data_connection_block.v
@@ -1,16 +1,36 @@
 module data_connection_block
   #(
+    // The number of fabric wires connecting to the MAC inputs.
+    // The number of fabric wires connecting to the MAC outputs.
     parameter W = 16, // W must be multiple of WW
+    // WW = WORD_WIDTH.
     parameter WW = 8,
-    parameter DATAIN = 3,
-    parameter DATAOUT = 2
+    // 8: Four pairs of WW-bits coming in.
+    parameter DATAIN = 8,
+    // 4: Four big chungus (4*WW) outputs.
+    parameter DATAOUT = 16,
+    parameter CONF_WIDTH = W*(DATAIN+DATAOUT)
     ) 
    (
+    input clk,
+    input rst,
+    input cset,
+    input [CONF_WIDTH-1:0] c,
+
     inout [W-1:0] 	       north, south,
+    // To MAC block.
     output [WW*DATAIN-1:0]     data_input,
-    input [WW*DATAOUT-1:0]     data_output,
-    input [W*(DATAIN+DATAOUT)-1:0] c
+    // From MAC block.
+    input [WW*DATAOUT-1:0]     data_output
     );
+
+   reg [CONF_WIDTH-1:0] c_reg;
+   always @(posedge clk) begin
+     if (rst)
+       c_reg <= {CONF_WIDTH{1'b0}};
+     else if (cset)
+       c_reg <= c;
+   end
    
    genvar 		       i, j;
    
@@ -21,13 +41,13 @@ module data_connection_block
       
       for(i = 0; i < DATAIN; i = i + 1) begin : inputs
 	 for(j = 0; j < W; j = j + 1) begin : switches
-	    transmission_gate_oneway s(data_input[j%WW+i*WW], north[j], c[j+i*W]);
+	    transmission_gate_oneway s(data_input[j%WW+i*WW], north[j], c_reg[j+i*W]);
 	 end
       end
       
       for(i = 0; i < DATAOUT; i = i + 1) begin : outputs
 	 for(j = 0; j < W; j = j + 1) begin : switches
-	    transmission_gate_oneway s(north[j], data_output[j%WW+i*WW], c[j+i*W+DATAIN*W]);
+	    transmission_gate_oneway s(north[j], data_output[j%WW+i*WW], c_reg[j+i*W+DATAIN*W]);
 	 end
       end
    endgenerate
--- src/disjoint_switch_box.v
+++ src/disjoint_switch_box.v
@@ -1,11 +1,26 @@
 module disjoint_switch_box 
   #(
-    parameter W = 8
+    // The number of fabric wires out each side.
+    parameter W = 8,
+    // There are 6 switches in each of the switch_box_element_ones.
+    parameter CONF_WIDTH = 6*W
     ) 
    (
+    input clk,
+    input rst,
+    input cset,
+    input [CONF_WIDTH-1:0] c,
+
     inout [W-1:0]   north, east, south, west,
-    input [W*6-1:0] c
     );
+
+   reg [CONF_WIDTH-1:0] c_reg;
+   always @(posedge clk) begin
+     if (rst)
+       c_reg <= {CONF_WIDTH{1'b0}};
+     else if (cset)
+       c_reg <= c;
+   end
    
    genvar 	    i;
    generate
@@ -16,7 +31,7 @@ module disjoint_switch_box
 	       .east(east[i]),
 	       .south(south[i]),
 	       .west(west[i]),
-	       .c(c[(i+1)*6-1:i*6])
+	       .c(c_reg[(i+1)*6-1:i*6])
 	       );
       end
    endgenerate
