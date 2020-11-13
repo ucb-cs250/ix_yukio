7e5de22f114558eba85946f203f8d16ec5a38be0
miyasaka <yoyuohlhjl@yahoo.co.jp>
2020-11-13 15:30:43 +0900

    fix bug in mux2

--- util/mux2.v
+++ util/mux2.v
@@ -2,8 +2,8 @@ module mux2(output reg  out, input in0, in1, input sel);
    
    always @(*) begin
       case(sel)
-	2'd0: out = in0;
-	2'd1: out = in1;
+	1'b0: out = in0;
+	1'b1: out = in1;
       endcase
    end
 
