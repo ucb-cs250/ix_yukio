156309664026d8c99ab3f48db53a2beae3d46a33
nqdtan <tan.nqd@gmail.com>
2020-12-02 00:51:08 -0800

    syntax fixed

--- src/transmission_gate_cell.v
+++ src/transmission_gate_cell.v
@@ -1,5 +1,5 @@
-module transmission_gate_cell(inout a, inout b, input c, input c_not);
+module transmission_gate_cell(inout A, inout B, input C, input Cnot);
   // Use the verilog primitive where available, in which case c_not is not
   // used.
-  tranif1(a, b, c);
+  tranif1(A, B, C);
 endmodule
